eb34fc32e5e9cf41a72ed2a201a7499c5570e7dc
Andrzej <Andrze@Pustelnik.com>
2022-04-23 11:23:02 +0200

    Fixing file name

--- flask_app.py
+++ flask_app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('start.html')
+    return render_template('index.html')
 
 @app.route('/rooms')
 def hello_world_rooms():
