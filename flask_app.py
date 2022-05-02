3619e0e1396c389c2740d113d9e5dd6722e6768b
ppporanczyk <piotr.poranczyk@gmail.com>
2022-05-02 21:53:10 +0200

    connected with DB, implemented filtering gamers

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,8 @@
+import os
 
-from flask import Flask,  request, redirect, render_template
+import psycopg2
+from flask import Flask, request, redirect, render_template
 from urllib.parse import urlparse, urlunparse
-
 
 app = Flask(__name__)
 
@@ -9,16 +10,38 @@ app = Flask(__name__)
 # FROM_DOMAIN = "statki.pythonanywhere.com"
 # TO_DOMAIN = "149.156.43.57/p23"
 
+def get_db_connection():
+    conn = psycopg2.connect(host='ec2-52-212-228-71.eu-west-1.compute.amazonaws.com',
+                            database='db1955gb2mvu3g',
+                            user='jslsutrxhbtyxw',
+                            password='4547e639209c8471045b50057d23b3f7f8911b88bcfd001fd87b7785ef3f543e')
+    # user = os.environ['DB_USERNAME'],
+    # password = os.environ['DB_PASSWORD'])
+    return conn
+
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
+
 @app.route('/rooms')
 def hello_world_rooms():
-    x = ''
-    str = ''
-    return 'We have no rooms yet'
+    return render_template('rooms.html',
+                           users=get_gamers(),
+                           available=get_gamers('available'),
+                           in_game=get_gamers('in_game'))
+
+
+def get_gamers(status=None):
+    connected_db = get_db_connection()
+    cur = connected_db.cursor()
+    formula = f'WHERE status=\'{status}\'' if status else ''
+    cur.execute(f'SELECT * FROM users {formula}')
+    gamers = cur.fetchall()
+    cur.close()
+    connected_db.close()
+    return gamers
 
 
 # @app.before_request
@@ -28,3 +51,6 @@ def hello_world_rooms():
 #         urlparts_list = list(urlparts)
 #         urlparts_list[1] = TO_DOMAIN
 #     return redirect(urlunparse(urlparts_list), code=301)
+
+if __name__ == '__main__':
+    app.run(debug=False)
