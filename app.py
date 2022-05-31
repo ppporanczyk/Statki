e09e15d95484cec7154f1935e4132ea8018a49ea
ppporanczyk <piotr.poranczyk@gmail.com>
2022-05-31 22:19:13 +0200

    wylistowanie innych zalogowanych graczy online

--- app.py
+++ app.py
@@ -10,13 +10,22 @@ from flask_login import LoginManager, UserMixin, login_required, login_user, log
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from pusher import pusher
+from enum import Enum
 
 SECRET_KEY = os.urandom(32)
 # FROM_DOMAIN = "statki.pythonanywhere.com"
 # TO_DOMAIN = "149.156.43.57/p23"
 
 app = Flask(__name__)
-
+# should be updated to working db URI to run on heroku
+pusher = pusher_client = pusher.Pusher(
+    app_id='1410664',
+    key='4d2726b6eaed69e2834f',
+    secret='3ca2a9d952ba6921320b',
+    cluster='eu',
+    ssl=True
+)
+name = ''
 app.config[
     'SQLALCHEMY_DATABASE_URI'] = 'postgresql://qbhjnrrwqvchoi:a12038c8f5d69267b00001db8cd0762c79458d0ec0cf8399467df7e04d1d8d50@ec2-34-242-8-97.eu-west-1.compute.amazonaws.com:5432/d9qk23pnab16ud'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -31,17 +40,52 @@ app.config['MAIL_PASSWORD'] = "okretyOkrety2"
 mail = Mail(app)
 
 db = SQLAlchemy(app)
+
+
+class Users(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), index=True, unique=True)
+    email = db.Column(db.String(150), unique=True, index=True)
+    password = db.Column(db.String(150))
+    status = db.Column(db.String(150))
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+
+class GameResult(Enum):
+    WON = 1
+    LOST = 2
+    NOT_CONCLUDED = 3
+
+
+class GameState(Enum):
+    IN_PROGRESS = 1
+    ENDED = 2
+    IN_PREPARATION = 3
+
+
+class Games(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    state = db.Column(db.String(50), unique=False)
+
+
+class PlayersGames(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    game_id = db.Column(db.Integer)
+    player_id = db.Column(db.Integer)
+    result = db.Column(db.String(50))
+
+
+db.create_all()
 login_manager = LoginManager()
 login_manager.init_app(app)
-
-pusher = pusher_client = pusher.Pusher(
-    app_id='1410664',
-    key='4d2726b6eaed69e2834f',
-    secret='3ca2a9d952ba6921320b',
-    cluster='eu',
-    ssl=True
-)
-name = ''
 
 
 def password_generator(len_of_password):
@@ -71,6 +115,8 @@ def main_page():
 @login_required
 @app.route('/play')
 def play():
+    global name
+    name = request.args.get('name')
     return render_template('play.html', name=current_user.name)
 
 
@@ -80,7 +126,7 @@ def pusher_authentication():
         channel=request.form['channel_name'],
         socket_id=request.form['socket_id'],
         custom_data={
-            u'user_id': name,
+            u'user_id': current_user.name,
             u'user_info': {
                 u'role': u'player'
             }
@@ -94,9 +140,25 @@ def load_user(user_id):
     return Users.query.get(user_id)
 
 
+def get_games_won_for_player():
+    pass
+
+
+def get_games_lost_for_player():
+    pass
+
+
+def get_games_in_progress_for_player():
+    pass
+
+
+@login_required
 @app.route('/profile')
 def profile():
-    return render_template('profile.html')
+    return render_template('profile.html',
+                           games_in_progress=get_games_in_progress_for_player(),
+                           games_won=get_games_won_for_player(),
+                           games_lost=get_games_lost_for_player())
 
 
 @login_required
@@ -179,21 +241,8 @@ def logout():
 #     return redirect(urlunparse(urlparts_list), code=301)
 
 
-class Users(UserMixin, db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), index=True, unique=True)
-    email = db.Column(db.String(150), unique=True, index=True)
-    password = db.Column(db.String(150))
-    status = db.Column(db.String(150))
-
-    def set_password(self, password):
-        self.password = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
-
-
 if __name__ == "__main__":
     app.debug = True
     app.run()
+
+name = ''
