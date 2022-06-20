bc906d2fd4c9fa61e4b515205b8ee1be44710e96
Wojciech Sękowski <wsekowski@poczta.onet.pl>
2022-06-20 18:37:32 +0200

    Naprawienie wysylania maili

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = "shipgamesender@gmail.com"
-app.config['MAIL_PASSWORD'] = "okretyOkrety2"
+app.config['MAIL_PASSWORD'] = "sjripnmdztjsrvoc"
 mail = Mail(app)
 
 db = SQLAlchemy(app)
